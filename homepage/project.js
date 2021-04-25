02ae72f26e6d40d5394595708de9db2ce5436535
Ngogiahuy1106 <ngogiahuy5se@gmail.com>
2021-04-25 16:43:28 +0700

    huy thich gai vu to

---
+++ homepage/project.js
@@ -0,0 +1,7 @@
+var dateObj = new Date();
+var month = dateObj.getUTCMonth() + 1; //months from 1-12
+var day = dateObj.getUTCDate();
+var year = dateObj.getUTCFullYear();
+
+newdate = year + "/" + month + "/" + day;
+document.getElementById("Time").textContent = newdate
