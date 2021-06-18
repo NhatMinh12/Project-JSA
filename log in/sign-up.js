5e2758569c0a71b70eb0a4254d7ec65a1585146b
duongvanhungnguoiban <duongthoikhong@gmail.com>
2021-06-19 00:09:37 +0700

    ok

--- log in/sign-up.js
+++ log in/sign-up.js
@@ -2,7 +2,8 @@ let inputEmail = document.getElementById("inputEmail");
 let inputPass = document.getElementById("inputPass");
 let inputcfPass = document.getElementById("inputcfPass");
 
-let signInBtn = document.getElementById("signUp");
+let checkingEmail = document.getElementById('checkingEmail')
+let signUpBtn = document.getElementById("signUp");
 
 let warnEmail = document.getElementById("warningEmail");
 let warnPass = document.getElementById("warningPass");
@@ -24,14 +25,16 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
 
-signInBtn.addEventListener("click", function () {
+signUpBtn.addEventListener("click", function () {
   let myEmail = inputEmail.value;
   let myPass = inputPass.value;
   let myCfPass = inputcfPass.value;
+  console.log(isDuplicatedAccount(myEmail))
   if (
     checkEmail(myEmail) == true &&
     checkPassword(myPass) == true &&
-    myPass === myCfPass
+    myPass === myCfPass &&
+    isDuplicatedAccount(myEmail) == false
   ) {
     db.collection("account")
       .add({
@@ -40,12 +43,17 @@ signInBtn.addEventListener("click", function () {
       })
       .then((docRef) => {
         // console.log("Document written with ID: ", docRef.id);
-        window.location.href = './log-in.html'
+        // window.location.href = './log-in.html'
     })
     .catch((error) => {
         // console.error("Error adding document: ", error);
     });
-  } 
+    warnEmail.style.display = 'none'
+    warnPass.style.display = 'none'
+  } else{
+    warnEmail.style.display = 'block'
+    warnPass.style.display = 'block'
+  }
 });
 
 function checkEmail(strEmail) {
@@ -53,6 +61,20 @@ function checkEmail(strEmail) {
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return checkEmail.test(strEmail);
 }
+
+// function validEmail(strEmail) {
+//   db.collection("account").where("email", "==", strEmail)
+//     .get()
+//     .then((querySnapshot) => {
+//         querySnapshot.forEach((doc) => {
+//           checkingEmail.classList.remove('ghost');
+//         });
+//     })
+//     .catch((error) => {
+//     });
+// }
+// let theEmail = inputEmail.value;
+// validEmail(theEmail)
 
 function checkPassword(strPassword) {
   if (strPassword.length >= 8) {
@@ -62,30 +84,51 @@ function checkPassword(strPassword) {
   }
 }
 
-inputEmail.addEventListener("keyup", function () {
-  let myEmail = inputEmail.value;
-  if (checkEmail(myEmail) == false) {
-    warnEmail.classList.remove("ghost");
-  } else {
-    warnEmail.classList.add("ghost");
-  }
-});
+function isDuplicatedAccount(newEmail){
+  db.collection("account").where("email", "==", newEmail)
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log(doc.id);
+          if(doc.id == ''){
+            return true
+          }
+        });
+        
+    })
+    .catch((error) => {
+      return false
+    });
+    return result
+}
 
-inputPass.addEventListener("keyup", function () {
-  let myPass = inputPass.value;
-  if (checkPassword(myPass) == false) {
-    warnPass.classList.remove("ghost");
-  } else {
-    warnPass.classList.add("ghost");
-  }
-});
+// console.log(isDuplicatedAccount('hehehe@hh.com'))
 
-inputcfPass.addEventListener("keyup", function () {
-  let myPass = inputPass.value;
-  let myCfPass = inputcfPass.value;
-  if (myPass !== myCfPass) {
-    warnCfPass.classList.remove("ghost");
-  } else {
-    warnCfPass.classList.add("ghost");
-  }
-});
+// inputPass.addEventListener("keyup", function () {
+//   let myPass = inputPass.value;
+//   if (checkPassword(myPass) == false) {
+//     warnEmail.style.display='block'
+//   } else {
+//     warnEmail.style.display='none'
+//   }
+// });
+
+// inputPass.addEventListener("keyup", function () {
+//   let myPass = inputPass.value;
+//   if (checkPassword(myPass) == false) {
+//     warnPass.classList.remove("ghost");
+//   } else {
+//     warnPass.classList.add("ghost");
+//   }
+// });
+
+// inputcfPass.addEventListener("keyup", function () {
+//   let myPass = inputPass.value;
+//   let myCfPass = inputcfPass.value;
+//   if (myPass !== myCfPass) {
+//     warnCfPass.classList.remove("ghost");
+//   } else {
+//     warnCfPass.classList.add("ghost");
+//   }
+// });
+
